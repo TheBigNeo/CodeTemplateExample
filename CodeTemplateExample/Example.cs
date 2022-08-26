204e5d9ea5016c7f75a0364d45aaac50968db4fd
TheBigNeo <alexandro.will@outlook.com>
2022-08-26 18:29:11 +0200

    ...

--- CodeTemplateExample/Example.cs
+++ CodeTemplateExample/Example.cs
@@ -6,13 +6,17 @@ public static class Example
     {
         Console.Out.WriteLine("Line 1");
 
+        // This way the hint is not displayed.
+        // But if you now call value.HasText() once, the hint is displayed. 
+        // If you delete value.HasText again and restart Rider, the hint is missing again.
+
         if (!string.IsNullOrWhiteSpace(value))
         {
-            Console.Out.WriteLine("No Value");
+            Console.Out.WriteLine("Value");
         }
         else
         {
-            Console.Out.WriteLine("Value");
+            Console.Out.WriteLine("No Value");
         }
     }
 }
--- CodeTemplateExample/Program.cs
+++ CodeTemplateExample/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using CodeTemplateExample;
+
 Console.WriteLine("Hello, World!");
+
+Example.Hello("Value");
